ac41cd20bed8519469ed01a72125e7044ae9895b
alyona <alyona.kirushcheva@gmail.com>
2023-02-07 13:18:00 +0100

    widget

--- app/code/Alyona/PostEAV/Block/Widget/Tags.php
+++ app/code/Alyona/PostEAV/Block/Widget/Tags.php
@@ -10,28 +10,28 @@ class Tags extends Template implements BlockInterface
     protected $tagRepository;
     protected $_template = "widget/tags.phtml";
 
-//    /**
-//     * @param \Magento\Framework\View\Element\Template\Context $context
-//     * @param \Alyona\PostEAV\Model\TagRepository $tagRepository
-//     * @param array $data
-//     */
-//    public function __construct(
-//        \Magento\Framework\View\Element\Template\Context $context,
-//        \Alyona\PostEAV\Model\TagRepository             $tagRepository,
-//        array                                            $data = []
-//    ) {
-//        $this->tagRepository = $tagRepository;
-//        parent::__construct($context, $data);
-//    }
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Alyona\PostEAV\Model\TagRepository $tagRepository
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Alyona\PostEAV\Model\TagRepository             $tagRepository,
+        array                                            $data = []
+    ) {
+        $this->tagRepository = $tagRepository;
+        parent::__construct($context, $data);
+    }
 
     public function returnTagsAsString()
     {
 
         $result="";
-//        $tags = $tagRepository->get();
-//        foreach ($tags->getItems() as $tag){
-//            $result.=$tag->getName()." ";
-//        }
+        $tags = $this->tagRepository->get();
+        foreach ($tags->getItems() as $tag){
+            $result.=$tag->getName()."\t";
+        }
         return trim($result);
     }
 }
