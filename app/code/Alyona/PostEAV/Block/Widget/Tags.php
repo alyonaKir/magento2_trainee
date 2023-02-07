e4731fbf9c0d007fcaddde911e9d55c212b462a5
alyona <alyona.kirushcheva@gmail.com>
2023-02-07 13:03:02 +0100

    widget

---
+++ app/code/Alyona/PostEAV/Block/Widget/Tags.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Alyona\PostEAV\Block\Widget;
+
+use Alyona\PostEAV\Model\TagRepository;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+class Tags extends Template implements BlockInterface
+{
+    protected $tagRepository;
+    protected $_template = "widget/tags.phtml";
+
+//    /**
+//     * @param \Magento\Framework\View\Element\Template\Context $context
+//     * @param \Alyona\PostEAV\Model\TagRepository $tagRepository
+//     * @param array $data
+//     */
+//    public function __construct(
+//        \Magento\Framework\View\Element\Template\Context $context,
+//        \Alyona\PostEAV\Model\TagRepository             $tagRepository,
+//        array                                            $data = []
+//    ) {
+//        $this->tagRepository = $tagRepository;
+//        parent::__construct($context, $data);
+//    }
+
+    public function returnTagsAsString()
+    {
+
+        $result="";
+//        $tags = $tagRepository->get();
+//        foreach ($tags->getItems() as $tag){
+//            $result.=$tag->getName()." ";
+//        }
+        return trim($result);
+    }
+}
